cbf2f311aee7acc0fabea2ab063ea5c76b6431ff
Soumya <soumya@Soumyas-Laptop.fritz.box>
2024-11-30 08:24:13 +0100

    updated pytest for ingredient

---
+++ recipe-app/tests/test_ingredient.py
@@ -0,0 +1,7 @@
+import pytest
+from app.ingredient import enter_ingredients
+
+def test_enter_ingredients(monkeypatch):    
+    monkeypatch.setattr('builtins.input', lambda _: "egg, milk, bread")
+    ingredients = enter_ingredients()
+    assert ingredients == {"egg", "milk", "bread"}
